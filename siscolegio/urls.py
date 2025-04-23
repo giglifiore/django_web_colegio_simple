fcd92cf3d7415c6ae0ea948de8f04cf2da3d3ba5
giglifiore <49178731+giglifiore@users.noreply.github.com>
2025-04-23 15:17:02 -0400

    Update urls.py

--- siscolegio/urls.py
+++ siscolegio/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import include
+from django.urls import path
 
 from django.contrib import admin
 
@@ -7,9 +8,11 @@ from inicio import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'siscolegio.views.home', name='home'),
-    url(r'^$', views.inicio, name="inicio_redirect"),
-    url(r'^ameliagallegos/', include('inicio.urls', namespace='inicio')),
-    url(r'^sisacademico/', include('sisacademico.urls', namespace='sisacademico')),
-    url(r'^admin/', include(admin.site.urls)),
+   
+   # url(r'^$', views.inicio, name="inicio_redirect"),
+    #url(r'^ameliagallegos/', include('inicio.urls', namespace='inicio')),
+    #url(r'^sisacademico/', include('sisacademico.urls', namespace='sisacademico')),
+    #url(r'^admin/', include(admin.site.urls)),
+    path('', views.index, name='index'),
+    path('alumnos/', views.alumnos, name='alumnos'),
 )
