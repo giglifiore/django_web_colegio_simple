c5d6614aebc0381071611efc35dfc9ed42a0e317
giglifiore <49178731+giglifiore@users.noreply.github.com>
2025-04-23 15:21:22 -0400

    Update urls.py

--- siscolegio/urls.py
+++ siscolegio/urls.py
@@ -10,9 +10,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
    
    # url(r'^$', views.inicio, name="inicio_redirect"),
-    #url(r'^ameliagallegos/', include('inicio.urls', namespace='inicio')),
-    #url(r'^sisacademico/', include('sisacademico.urls', namespace='sisacademico')),
-    #url(r'^admin/', include(admin.site.urls)),
+    path('ameliagallegos/', include('inicio.urls', namespace='inicio')),
+    path('sisacademico/', include('sisacademico.urls', namespace='sisacademico')),
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('alumnos/', views.alumnos, name='alumnos'),
 )
