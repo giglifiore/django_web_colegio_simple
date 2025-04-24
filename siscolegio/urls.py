a6ad9b62a1a8e78a4cb3788c26b5f0dfa5f9e3e7
giglifiore <49178731+giglifiore@users.noreply.github.com>
2025-04-24 10:12:56 -0400

    Update urls.py

--- siscolegio/urls.py
+++ siscolegio/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from sisacademico import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio, name='inicio'),
+    path('', views.inicio, name='inicio'),
     #path('', include('inicio.urls', namespace='inicio')),  # Página de inicio
     path('ameliagallegos/', include('inicio.urls', namespace='inicio')),
     path('sisacademico/', include('sisacademico.urls', namespace='sisacademico')),
